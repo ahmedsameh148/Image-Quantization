8cb88ad0b3e57b387d34d5db89c21f078825405a
AhmedRagab99 <47069606+AhmedRagab99@users.noreply.github.com>
2019-12-10 19:56:48 +0200

    added Dsu complexity

--- ImageQuantization/Dsu.cs
+++ ImageQuantization/Dsu.cs
@@ -1,50 +1,48 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-
-namespace ImageQuantization
+class DSU
 {
-    class DSU
+
+    public int[] p;// O(1)
+    public int[] rank;//O(1)
+
+
+    public DSU()
     {
-        public int[] p;
-        public int[] rank;
-        
-        public DSU()
-        {
-            p = new int[100005];
-            rank = new int[100005];
-        }
 
-        public int FindParent(int node)
-        {
-            if (p[node] == node)
-                return node;
-            return p[node] = FindParent(p[node]);
-        }
+        p = new int[100005]; //O(1)
+        rank = new int[100005];//O(1)
+    }
 
-        public void connect(int node1, int node2)
-        {
-            int p1 = FindParent(node1), p2 = FindParent(node2);
-            if (rank[p1] >= rank[p2])
-            {
-                p[p2] = p1;
-                rank[p1] += rank[p2];
-            }
-            else
-            {
-                p[p1] = p2;
-                rank[p2] += rank[p1];
-            }
-        }
+    public int FindParent(int node)
+    {
+        if (p[node] == node)  //O(1)
+            return node;  //O(1)
+        return p[node] = FindParent(p[node]);  //O(log*(n))
+    }
 
-        public void init()
+    public void connect(int node1, int node2)
+    {
+        int p1 = FindParent(node1), p2 = FindParent(node2); //both take theta of (1)
+        if (rank[p1] >= rank[p2]) //O(1)
         {
-            for (int i = 0; i < 10005; i++)
-            {
-                rank[i] = 1;
-                p[i] = i;
-            }
+            p[p2] = p1; //O(1)
+            rank[p1] += rank[p2]; //O(1)
+        }
+        else
+        {
+            p[p1] = p2; //theta (1)
+            rank[p2] += rank[p1];//theta(1)
+        }
+    }
+
+    public void init()
+    {
+        //theta(100005) so takes O(1)
+        for (int i = 0; i < 100005; i++)
+        {
+            //theta(1)
+            rank[i] = 1;
+            //theta(1)
+            p[i] = i;
         }
     }
 }
